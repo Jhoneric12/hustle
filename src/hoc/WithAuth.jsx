ed974e48bb902602ae1273355bc3db3ba8bd166c
unknown <jhonerix23@gmail.com>
2024-07-17 19:12:20 +0800

    removed bugs in higher order component

--- src/hoc/WithAuth.jsx
+++ src/hoc/WithAuth.jsx
@@ -3,27 +3,25 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function withAuth(WrappedComponent) {
+export default function withAuth (WrappedComponent) {
 
-    const WithAuth = (props) => {
-        const router = useRouter();
+    return (props) => {
+
+        const router = useRouter()
         const [isAuthenticated, setIsAuthenticated] = useState(false);
 
         useEffect(() => {
-            const user = JSON.parse(localStorage.getItem('user'));
+            const user = JSON.parse(localStorage.getItem('user'))
 
             if (!user) {
-                setIsAuthenticated(false);
-                router.push('/login');
-            } else {
-                setIsAuthenticated(true);
+                setIsAuthenticated(false)
+                router.push('/login')
             }
-        }, [router]);
+            else {
+                setIsAuthenticated(true)
+            }
+        }, [router])
 
-        return isAuthenticated ? <WrappedComponent {...props} /> : null;
-    };
-
-    WithAuth.displayName = `WithAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
-
-    return WithAuth;
+        return isAuthenticated ?  <WrappedComponent {...props} /> : null
+    }
 }
